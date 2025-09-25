34e82cbb40e5098a875f344c0608f4a309e57a17
Eric Tao <shuaibtcm@gmail.com>
2025-09-25 15:27:18 +0100

    Original Version of Kitto with All demo Commented

---
+++ Kitto/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  Kitto
+//
+//  Created by Toby Miles on 24/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View
+    {
+        VStack
+        {
+            Button("Hello Kitto"){
+                
+            }
+            .background(Color.white)
+            .foregroundColor(.black)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+        }
+        
+    }
+    /*var body: some View
+    {
+        NavigationSplitView
+        {
+            List
+            {
+                ForEach(items)
+                { item in
+                    NavigationLink
+                    {
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                    } label:
+                    {
+                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar
+            {
+                ToolbarItem(placement: .navigationBarTrailing)
+                {
+                    EditButton()
+                }
+                ToolbarItem
+                {
+                    Button(action: addItem)
+                    {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        } detail:
+        {
+            Text("Select an item")
+        }
+    }
+    */
+
+/*    private func addItem()
+    {
+        withAnimation
+        {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+*/}
+
+#Preview
+{
+    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+}
