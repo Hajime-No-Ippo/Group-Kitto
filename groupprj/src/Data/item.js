808207b2350e78fc26a62e92d7aa7c7abc10f6a2
EvaJ <jxtevaie@gmail.com>
2025-11-19 15:58:21 +0000

    table to link product and pict

---
+++ groupprj/src/Data/item.js
@@ -0,0 +1,52 @@
+const Products = [
+  {
+    productID:"6ab67d95-1c6e-41a3-b6f0-c234f617f3d3",
+    img: ["/img/vintage-jacket.jpg"],
+  },
+  {
+    productID:"80df0971-1ea8-463a-9f52-a3840a53a524",
+    img: ["/img/sneakers.jpg"],
+  },
+  {
+    productID:"fdb16eda-0b68-467e-9f69-88f6a1279b79",
+    img: ["/img/black-backpack.jpg"],
+  },
+  {
+    productID:"ace77b59-721c-4555-8f3a-5e59e1c51de5",
+    img: ["/img/ceramic-handmade-mug.jpg"],
+  },
+  {
+    productID:"96b11aa6-ba13-48fa-ac8b-e5d32a493797",
+    img: ["/img/laptop-stand.jpg"],
+  },
+  {
+    productID:"f4a821cc-47a3-4df7-836c-fbd9efbe5ce1",
+    img: ["/img/desk-lamp.jpg"],
+  },
+  {
+    productID:"251ab390-0a67-4dd2-9e5e-c2cd79e676cc",
+    img: ["/img/headphones.jpg"],
+  },
+  {
+    productID:"d80d3ad4-2e05-48b5-86f2-9bb793c759db",
+    img: ["/img/chair.jpg"],
+  },
+  {
+    productID:"438cd129-b54a-46ec-bbad-b05354359df9",
+    img: ["/img/bottle-black-eco.jpg"],
+  },
+  {
+    productID:"c9b62719-677d-4987-952e-0722d9925bd1",
+    img: ["/img/tote-bag.jpg"],
+  },
+  {
+    productID:"cb74a3d7-2ae0-46ba-9359-4e74137497bc",
+    img: ["/img/bluetooth-speaker.jpg"],
+  },
+  {
+    productID:"4bb5a045-8bcc-4c74-8820-f85f9aab9d94",
+    img: ["/img/indoor-plant.jpg"],
+  },
+];
+
+export default Products;
