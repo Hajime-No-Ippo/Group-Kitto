788cb1e02fddab56e0c142186c39439230854d34
RileyWang7 <rileywang2003@gmail.com>
2025-11-27 19:13:24 +0000

    edit nav

--- groupprj/src/component/nav.jsx
+++ groupprj/src/component/nav.jsx
@@ -1,4 +1,3 @@
-// src/component/nav.jsx
 import React from "react";
 import { logout } from "../service/authService";
 import { useNavigate } from "react-router-dom";
@@ -9,8 +8,6 @@ const Nav = () => {
     e.preventDefault();
     window.location.href = "/userProfile";
   };
-
-  const navigate = useNavigate();
 
   const handleLogOut = async (e) => {
     e.preventDefault();
@@ -24,40 +21,34 @@ const Nav = () => {
 
   return (
     <nav className="w-full bg-black text-white px-8 py-3 flex justify-between shadow">
-      {/* Logo - click this logo always go to home */}
       <a
-        className="text-2xl navbar-brand font-semibold justify-start no-underline item-center
-"
-        href="/"
+        href="/home"
+        className="text-2xl font-bold tracking-wide no-underline text-white hover:opacity-80 transition"
       >
-        Kitto
-        <span className="font-extralight ml-1">Market</span>
+        Kitto<span className="font-light ml-1">Market</span>
       </a>
-      <div className="justify-between flex space-x-7">
-        {/* Right side button */}
+
+      <div className="flex space-x-7">
+        {/* My Profile */}
         <a
-          className=" font-extralight flex gap-1 text-white no-underline hover:underline item-center mt-1
-"
-          href="/profile"
+          className="font-extralight text-white no-underline hover:underline mt-1"
+          href="/userProfile"
           onClick={handleOnclick}
         >
           My Profile
         </a>
 
-        {/* Right side button */}
+        {/* My Favorites */}
         <a
-          className=" font-extralight flex  gap-1 no-underline hover:underline text-white item-center mt-1
-"
+          className="font-extralight text-white no-underline hover:underline mt-1"
           href="/likeit"
-          onClick={handleOnclick}
         >
-          My Likes
+          My Favorites
         </a>
 
-        {/* Sign out button */}
+        {/* Sign Out */}
         <a
-          className="font-extralight flex gap-1 no-underline hover:underline text-white item-center mt-1
-"
+          className="font-extralight text-white no-underline hover:underline mt-1"
           href="/"
           onClick={handleLogOut}
         >
